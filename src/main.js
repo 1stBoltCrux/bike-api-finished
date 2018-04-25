7aa16b4dedea962e0aa125ab3a41dc31a87629e2
Brea Borlas and Ted Leary <brea+ted@>
2018-04-25 11:45:15 -0700

    Format output of response

--- src/main.js
+++ src/main.js
@@ -9,11 +9,12 @@ $(document).ready(function(){
     event.preventDefault();
     let zip = $('#zipInput').val();
     $('#zipInput').val("");
+    let distance = $('#distanceInput').val();
+    $('#distanceInput').val("");
+    let manufacturer = $('#manufacturerInput').val();
+    $('#manufacturerInput').val("");
     $.ajax({
-      url: `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=trek&location=${zip}&distance=10&stolenness=stolen&access_token=cors`,
-      // headers: {
-      //   "Access-Control-Allow-Origin": "*"
-      // },
+      url: `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&manufacturer=${manufacturer}&location=${zip}&distance=${distance}&stolenness=stolen&access_token=cors`,
       type: 'GET',
       data: {
         format: 'json'
@@ -22,7 +23,7 @@ $(document).ready(function(){
         let bikeArray = response.bikes;
         $('#output').empty();
         bikeArray.forEach(function(bike) {
-          $('#output').append(bike.manufacturer_name);
+          $('#output').append(`<ul><li>${bike.title}</li><li>${bike.stolen_location}</li><li>${bike.frame_colors}</li><ul>`);
         });
         //$('#output').text();
       },
